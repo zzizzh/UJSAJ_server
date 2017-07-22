43f658f1ddb505c9630ab21b5a631cc75fb184fd
zzizzh <zzizzh@nate.com>
2017-07-22 18:02:21 +0900

    server-client test

--- src/test/ClientControl.java
+++ src/test/ClientControl.java
@@ -13,83 +13,76 @@ import ProblemDomain.User;
  */
 public class ClientControl {
 
-	private ArrayList<Posts> timeLine;		// timeLine에 보일 Posts리스트
-	private ArrayList<Posts> myPostsList;	// 내가 쓴 Posts 리스트
-	private ArrayList<Posts> myLikeList;	// 내가 좋아요 한 Posts 리스트
-	private ArrayList<String> stringList;	// 통신 관련 string 리스트
-	
-	private User me;						// 현재 로그인 유저 정보
-	
-	
+	private ArrayList<Posts> timeLine; // timeLine에 보일 Posts리스트
+	private ArrayList<Posts> myPostsList; // 내가 쓴 Posts 리스트
+	private ArrayList<Posts> myLikeList; // 내가 좋아요 한 Posts 리스트
+	private ArrayList<String> stringList; // 통신 관련 string 리스트
+
+	private User me; // 현재 로그인 유저 정보
+
 	/*
-	 * 기본 생성자
-	 * ArrayList만 초기화
+	 * 기본 생성자 ArrayList만 초기화
 	 */
-	public ClientControl()
-	{
-		timeLine=new ArrayList<Posts>();
-		myPostsList=new ArrayList<Posts>();
-		stringList=new ArrayList<String>();
-		
+	public ClientControl() {
+		timeLine = new ArrayList<Posts>();
+		myPostsList = new ArrayList<Posts>();
+		stringList = new ArrayList<String>();
+
 		me = null;
-	}	
-	
+	}
+
 	/*
 	 * 서버에서 받아온 타임라인에 보일 PostsList를 저장.
 	 */
-	public void addTimeLine(PostsList postsList)
-	{
+	public void addTimeLine(PostsList postsList) {
 		timeLine.addAll(postsList.getAll());
 	}
 
 	/*
 	 * 서버에서 받아온 나의 PostsList를 저장.
 	 */
-	public void setMyPostsList(PostsList postsList)
-	{
+	public void setMyPostsList(PostsList postsList) {
 		myPostsList.addAll(postsList.getAll());
 	}
+
 	/*
 	 * 내가 쓴 글을 MyPostsList에 저장.
 	 */
-	public void addMyPostsList(Posts posts)
-	{
+	public void addMyPostsList(Posts posts) {
 		myPostsList.add(posts);
 	}
-	
+
 	/*
 	 * 서버에서 받아온 내가 좋아요 한 PostsList를 저장.
 	 */
-	public void setLikeList(PostsList postsList)
-	{
+	public void setLikeList(PostsList postsList) {
 		myLikeList.addAll(postsList.getAll());
 	}
+
 	/*
 	 * 내가 좋아요 한 글을 PostsList에 저장.
 	 */
-	public void addLikeList(Posts posts)
-	{
+	public void addLikeList(Posts posts) {
 		myLikeList.add(posts);
 	}
-	
-	
-	public void addString(String string)
-	{
+
+	/*
+	 * 
+	 */
+	public void setMe(User user) {
+		me = user;
+	}
+
+	public void addString(String string) {
 		stringList.add(string);
 	}
 
-	public void setMe(User user)
-	{
-		me = user;
-	}
-	
-	public void resetAll()
-	{
-		stringList=new ArrayList<String>();
-		timeLine=new ArrayList<Posts>();
-		myPostsList=new ArrayList<Posts>();
-		stringList=new ArrayList<String>();
-		
+	public void resetAll() {
+		stringList = new ArrayList<String>();
+		timeLine = new ArrayList<Posts>();
+		myPostsList = new ArrayList<Posts>();
+		stringList = new ArrayList<String>();
+
 		me = null;
 	}
 }
